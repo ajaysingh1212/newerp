3cd8d4dc5ee015ff4a96a044b71c1f245d0632d9
Sanket Kumar <ssbkumar5513@gmail.com>
2026-08-12 13:52:36 +0530

    kjh

--- app/Http/Controllers/Api/V1/Admin/RedeemCodeApiController.php
+++ app/Http/Controllers/Api/V1/Admin/RedeemCodeApiController.php
@@ -206,92 +206,10 @@ class RedeemCodeApiController extends Controller
         */
 
         return response()->json([
-            'success' => true,
-            'valid' => true,
-            'message' => 'Redeem code is valid.',
-
-            'data' => [
-
-                /*
-                |--------------------------------------------------------------------------
-                | Redeem Code
-                |--------------------------------------------------------------------------
-                */
-
-                'redeem_code' => [
-                    'id' => $redeemCode->id,
-                    'code' => $redeemCode->code,
-
-                    'recharge_plan_id' => $redeemCode->recharge_plan_id,
-
-                    'valid_up_to' => $redeemCode->valid_up_to,
-
-                    'discount_type' => $redeemCode->discount_type,
-                    'discount_value' => (float) $redeemCode->discount_value,
-
-                    // Actual amount that will be deducted
-                    'discount_amount' => $discountAmount,
-
-                    'status' => $redeemCode->status,
-                    'use_status' => $redeemCode->use_status,
-
-                    'used_by_id' => $redeemCode->used_by_id,
-                    'recharge_request_id' => $redeemCode->recharge_request_id,
-                    'used_at' => $redeemCode->used_at,
-
-                    'created_by_id' => $redeemCode->created_by_id,
-                    'creator_name' => $redeemCode->creator_name,
-                    'creator_role' => $redeemCode->creator_role,
-
-                    'created_at' => $redeemCode->created_at,
-                    'updated_at' => $redeemCode->updated_at,
-                    'deleted_at' => $redeemCode->deleted_at,
-                ],
-
-
-                /*
-                |--------------------------------------------------------------------------
-                | Recharge Plan
-                |--------------------------------------------------------------------------
-                */
-
-                'recharge_plan' => [
-                    'id' => $rechargePlan->id,
-                    'type' => $rechargePlan->type,
-                    'plan_name' => $rechargePlan->plan_name,
-
-                    'price' => (float) $rechargePlan->price,
-
-                    'subscription_duration' =>
-                        $rechargePlan->subscription_duration,
-
-                    'amc_duration' =>
-                        $rechargePlan->amc_duration,
-
-                    'warranty_duration' =>
-                        $rechargePlan->warranty_duration,
-
-                    'discription' =>
-                        $rechargePlan->discription,
-                ],
-
-
-                /*
-                |--------------------------------------------------------------------------
-                | Amount
-                |--------------------------------------------------------------------------
-                */
-
-                'amount' => [
-                    'plan_amount' => $planPrice,
-
-                    // Amount which will be deducted
-                    'discount_amount' => $discountAmount,
-
-                    // Final amount customer will pay
-                    'payable_amount' => $payableAmount,
-                ]
-            ]
-        ]);
+    'success' => true,
+    'valid' => true,
+    'message' => 'Redeem code is valid.',
+    'redeem_amount' => (float) $redeemCode->discount_amount,
+]);
     }
 }
